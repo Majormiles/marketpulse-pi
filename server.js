fd633107e550f07062a7b3864a0e8e3e96615a68
Majoemyles <majormilesvigour@gmail.com>
2024-12-06 19:23:55 +0000

    added bcrypt file

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const blogRoutes = require('./routes/blogRoutes'); // Import the blog routes
 
 const {
