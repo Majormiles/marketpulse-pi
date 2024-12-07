374135ca61cc6e9fa8248d7e517f98906a9a95c5
Majoemyles <majormilesvigour@gmail.com>
2024-12-07 14:47:17 +0000

    update

--- controllers/payment/paymentController.js
+++ controllers/payment/paymentController.js
@@ -6,52 +6,61 @@ const withdrowRequest = require('../../models/withdrowRequest')
 const { responseReturn } = require('../../utiles/response')
 const { mongo: { ObjectId } } = require('mongoose')
 const { v4: uuidv4 } = require('uuid')
+
 const stripe = require('stripe')('sk_test_51Nk8Y4F0B89ncn3xMHxYCwnaouDR6zuX83ckbJivv2jOUJ9CTka6anJcKMLnatgeBUeQq1RcRYynSPgp6f5zS4qF00YZFMYHuD')
 class paymentController {
     create_stripe_connect_account = async (req, res) => {
-        const { id } = req
-        const uid = uuidv4()
-
+        const { id } = req;
+        const uid = uuidv4();
+    
         try {
-            const stripInfo = await striptModel.findOne({ sellerId: id })
-
+            const stripInfo = await striptModel.findOne({ sellerId: id });
+    
+            let accountLink;
             if (stripInfo) {
-                await striptModel.deleteOne({ sellerId: id })
-                const account = await stripe.accounts.create({ type: 'express' })
-
-                const accountLink = await stripe.accountLinks.create({
+                await striptModel.deleteOne({ sellerId: id });
+                const account = await stripe.accounts.create({ type: 'express' });
+    
+                accountLink = await stripe.accountLinks.create({
                     account: account.id,
-                    refresh_url: 'https://marketpulse-oxxa.onrender.com/refresh',
-                    return_url: `https://marketpulse-oxxa.onrender.com/success?activeCode=${uid}`,
+                    refresh_url: 'https://marketpulse-api.onrender.com/refresh',
+                    return_url: `https://marketpulse-api.onrender.com/success?activeCode=${uid}`,
                     type: 'account_onboarding'
-                })
+                });
+    
                 await striptModel.create({
                     sellerId: id,
                     stripeId: account.id,
                     code: uid
-                })
-                responseReturn(res, 201, { url: accountLink.url })
+                });
             } else {
-                const account = await stripe.accounts.create({ type: 'express' })
-
-                const accountLink = await stripe.accountLinks.create({
+                const account = await stripe.accounts.create({ type: 'express' });
+    
+                accountLink = await stripe.accountLinks.create({
                     account: account.id,
-                    refresh_url: 'https://marketpulse-oxxa.onrender.com/refresh',
-                    return_url: `https://marketpulse-oxxa.onrender.com/success?activeCode=${uid}`,
+                    refresh_url: 'http://localhost:3001/refresh',
+                    return_url: `http://localhost:3001/success?activeCode=${uid}`,
                     type: 'account_onboarding'
-                })
+                });
+    
                 await striptModel.create({
                     sellerId: id,
                     stripeId: account.id,
                     code: uid
-                })
-                responseReturn(res, 201, { url: accountLink.url })
+                });
             }
+    
+            console.log("Stripe account link URL: ", accountLink.url);
+            responseReturn(res, 201, { url: accountLink.url });
         } catch (error) {
-            console.log('stripe connect account create error ' + error.message)
+            console.error("Stripe connect account create error: ", error);
+            responseReturn(res, 500, { message: 'Internal server error', error: error.message });
         }
     }
+    
 
+
+    
     active_stripe_connect_account = async (req, res) => {
         const { activeCode } = req.params
         const { id } = req
